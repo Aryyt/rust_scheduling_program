9f2be66c8ddb0114ee728673cafc1d044a1a78e2
artur <artur10.czaban@gmail.com>
2025-02-11 15:06:51 +0100

    Working but needs validation

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,58 @@
 use interval::Interval;
-use core::{panic};
-use std::collections::btree_map::Range;
+use core::panic;
+use std::cmp::max;
 use std::fs::File;
-use std::ops::Index;
-use std::{i32, path, string, usize};
+use std::{i32, usize, vec};
 use std::io::{BufRead, BufReader};
 
 mod interval;
 fn main() {
     let path:String = "src/weightedIntervals.txt".to_owned();
-    let mut intervals: Vec<Interval> = get_intervals_sorted_finishing(path);
-    let temp = get_predecessor(0, intervals[0].start(), &intervals);
-    println!("Hello, world! hope this works {}", temp);
+    let intervals: Vec<Interval> = get_intervals_sorted_finishing(path);
+    let mut sum = 0;
+    for interval in intervals.iter(){
+        sum += interval.weight()
+    }
+    println!("Sum of all weights is {}", sum);
+    schedueling_algo(intervals);
 }
 
 fn schedueling_algo(intervals: Vec<Interval>) {
-
-    for (index, Interval) in intervals.iter().enumerate(){
-
+    let mut m = vec![0; intervals.len()];
+    let mut k = vec![-1; intervals.len()];
+    for (index, _) in intervals.iter().enumerate(){
+        get_and_save_score(index, &intervals, &mut m, &mut k);
     }
 
+    let mut max_val = -1;
+    for (_, val) in m.iter().enumerate(){
+        if *val >= max_val{
+            max_val = *val
+        }
+    }
+    println!("Max of non overlaping set is {}", max_val);
+}
+
+fn get_and_save_score(index: usize, intervals: &Vec<Interval>, m: &mut Vec<i32>, k: &mut Vec<i32>){
+    //First Case
+    let predecessor = get_predecessor(index, intervals[index].start(), intervals);
+    let mut si: i32 = intervals[index].weight();
+    if predecessor >= 0{
+        si += m[predecessor as usize];
+    }
+    //Second Case (Max of previous interval)
+    let mut ni = 0;
+    if index > 0{
+        ni = m[index - 1];
+    }
+
+    let max_val = max(si, ni);
+    m[index] = max_val;
+    if max_val == si{
+        k[index] = predecessor
+    }else{
+        k[index] = (index - 1) as i32
+    }
 }
 
 fn get_predecessor(index: usize, starting_time: i32, intervals: &Vec<Interval>) -> i32 {
